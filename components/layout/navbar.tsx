77f50bfd72a692dc32c775832ee5605002057ce0
ammaarKhan13 <147348966+ammaarKhan13@users.noreply.github.com>
2025-03-23 11:30:00 +0530

    Update navbar.tsx

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -40,8 +40,8 @@ const routeList: RouteProps[] = [
     label: "Medicine GPT",
   },
   {
-    href: "/diseasedetector",
-    label: "AI Disease Detector",
+    href: "/assistant",
+    label: "MedBuddy",
   }
 ];
 
@@ -51,7 +51,7 @@ export const Navbar = () => {
     <header className="shadow-inner bg-opacity-15 w-[90%] md:w-[70%] lg:w-[75%] lg:max-w-screen-xl top-5 mx-auto sticky border border-secondary z-40 rounded-2xl flex justify-between items-center p-2 bg-card">
       <Link href="/" className="font-bold text-lg flex items-center">
         <ChevronsDown className="bg-gradient-to-tr border-secondary from-primary via-primary/70 to-primary rounded-lg w-9 h-9 mr-2 border text-white" />
-        SehatSaathi
+        Sehat Saathi
       </Link>
       {/* <!-- Mobile --> */}
       <div className="flex items-center lg:hidden">
@@ -72,7 +72,7 @@ export const Navbar = () => {
                 <SheetTitle className="flex items-center">
                   <Link href="/" className="flex items-center">
                     <ChevronsDown className="bg-gradient-to-tr border-secondary from-primary via-primary/70 to-primary rounded-lg w-9 h-9 mr-2 border text-white" />
-                    MedNourish AI
+                    Sehat Saathi
                   </Link>
                 </SheetTitle>
               </SheetHeader>
